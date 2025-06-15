fda09602e390e1bc5e6b0bd05b86bc92e5406341
Mayank Mittal <Mayankmittal@outlook.in>
2025-06-15 21:08:31 +0530

    feat: Added Broadcast Channel's

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Constants\Role;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('user.{id}', function ($user, $id) {
+    return ((int) $user->id === (int) $id && $user->role == Role::User);
+});
+
+Broadcast::channel('supplier.{id}', function ($user, $id) {
+    return ((int) $user->id === (int) $id && $user->role == Role::Provider);
 });
